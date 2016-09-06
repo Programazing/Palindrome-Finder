9f370534313248c3f89b193f06eb11c4542097fb
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-09-05 20:10:52 -0400

    Create Palindrome logic. It's simple enough that other commits were not needed.

--- Palindrome-Finder/Main.cs
+++ Palindrome-Finder/Main.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
-namespace Palindrome_Finder
+namespace PalindromeFinder
 {
-    public class Main
+    public class FindPalindrome
     {
+   
+        public bool isPalindrome(string stringToCheck, string charctersToIgnore)
+        {
+            stringToCheck = stringToCheck.ToLower();
+            string sanitizedInput = Regex.Replace(stringToCheck, @"\s|["+ charctersToIgnore +"]", "");
+            
+            int reverseStringLocation = sanitizedInput.Length -1;
 
+            for (int i = 0; i != reverseStringLocation; i++, reverseStringLocation--)
+            {
+                if(sanitizedInput[i] == sanitizedInput[reverseStringLocation])
+                {
+
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
